2436cbf1a4d85cef9f7e0a908dcca1820b5c3b33
Hernan <zerovirus23@gmail.com>
2016-06-12 01:19:05 -0500

    Added the required cars header to work with different origins

--- src/main/java/co/zero/vogue/resource/UtilsResource.java
+++ src/main/java/co/zero/vogue/resource/UtilsResource.java
@@ -3,12 +3,12 @@ package co.zero.vogue.resource;
 import co.zero.vogue.common.type.ProbabilityType;
 import co.zero.vogue.common.type.EventType;
 import co.zero.vogue.common.type.SeverityType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by htenjo on 5/29/16.
  */
+@CrossOrigin
 @RestController
 public class UtilsResource {
     @RequestMapping("/utils/reportTypes")
